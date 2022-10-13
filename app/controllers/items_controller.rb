e4cdafcdbc4fc947813d67c2a26720eb37df1700
ONIKIE <whiteriot0024@gmail.com>
2022-10-13 23:18:10 +0900

    アイテム一覧表示編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
 
 
@@ -10,26 +12,61 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+  
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
+    tag_list = params[:item][:tag_ids].split(',')
     if @item.save
+      @item.save_tags(tag_list)
       redirect_to root_path
     else
-      render :new
+      render 'new'
     end
   end
 
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    tag_list = params[:item][:tag_ids].split(',')
+    if @item.update_attributes(item_params)
+      @item.save_tags(tag_list)
+      redirect_to @item
+    else
+    render 'edit'
+    end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect :show
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:category_id, :type_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:category_id, :type_id, :image,{ tag_ids: [] }).merge(user_id: current_user.id)
   end
 
+
+  def set_product
+    @item = Item.find(params[:id])
+  end
+
+  def ensure_correct_user
+    redirect_to root_path unless @item.user.id == current_user.id
+  end
  
 end
 
